460adae5f51a7d9623072d2098bc3896e3d401dc
Gael DEBOST <zzgael@gmail.com>
2018-08-01 16:22:19 +0200

    Update SuperPDO.php

--- src/SuperPDO.php
+++ src/SuperPDO.php
@@ -3,16 +3,25 @@
 // Définition de la class qui hérite de PDO
 class SuperPDO 
 {
+    /***
+    * L'attribut qui stockera l'instance unique, la propriété est 
+    * statique car on ne veut stocker qu'une seule instance PDO
+    */
     private static $pdo; 
 
-    // Function connect avec le paramètre config
+    /*** 
+    * Fonction de connexion avec le paramètre config
+    */
     static public function connect($config) {
         static::$pdo = new \PDO($config["dsn"], $config["user"], $config["password"]);
 
         // Par défaut MySQL va désormais retourner des erreurs fatales
         static::$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
-    // Fonction permettant de génériser la connexion
+    /***
+    * Fonction permettant de génériser l'appel aux requêtes 
+    * préparées et non préparées
+    */
     static public function query($sql, $parameters = false) {
         if($parameters) {
             if(!is_array($parameters)) {
